c9af677a98a05ced27a1864f14ef9900391c76cd
Leonel <lolo042@hotmail.com>
2019-04-06 19:07:18 -0300

    Primer Test instanciando un gps, con la clase Gps

---
+++ UCPDeliberyAirGps/test/TestGPS.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import deliveryGps.Gps;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Juan Carlos
+ */
+public class TestGPS {
+    
+    public TestGPS() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    @Test
+    
+    public void crearGps(){
+    
+        Gps gps1 = new Gps("");
+        
+        assertNotNull(gps1);
+        
+        
+        
+        
+     
+        
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
