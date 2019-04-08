e551dd60f828e5d3cd548b2f3cd231e7fbd28799
AguirreAgustin <ardagustin@gmail.com>
2019-04-08 13:37:35 -0300

    Reubicación de varios métodos de la prueba unitaria de TestGPS en TestDrone

--- UCPDeliberyAirGps/src/deliveryGps/Drone.java
+++ UCPDeliberyAirGps/src/deliveryGps/Drone.java
@@ -31,7 +31,17 @@ public class Drone {
     public void agregarGPS(Gps pGps){
         this.getGps().add(pGps);
     }
-    
+    public String obtenerPosicionActual(){
+        
+        int i;
+        String posicionActual = "";
+        for(i=0;i<this.getGps().size();i++){
+            
+            posicionActual = this.getGps().get(i).getPath();
+            
+        }
+        return posicionActual;
+    }
     /*public ArrayList obtenerListadoPath(){
     
         this.getGps().l
